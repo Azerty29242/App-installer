ecd310ae0be608de924d353e867a153ecc25e9b3
Azerty29242 <jung.jules@pm.me>
2023-01-12 19:28:04 +0100

    Test apps.json parsing

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
 function getApps(callback) {
-    fetch(new URL("apps.json"))
+    fetch("https://raw.githubusercontent.com/Azerty29242/App-installer/main/apps.json")
         .then((response) => response.json())
-        .then((data) => {
-            callback(JSON.parse(data))
-            return true
+        .then((apps) => {
+            apps.forEach((app, index) => {
+                getLatestRelease(app.name, app.author, (release) => {
+                    app.latest = { 
+                        release: release
+                    }
+                    callback(app)
+                })
+            })
         })
 }
 
-getApps((data) => console.log(data))
+function getLatestRelease(name, author, callback) {
+    fetch(`https://api.github.com/repos/${author}/${name}/releases`)
+        .then((response) => response.json())
+        .then((data) => {
+            try {
+                callback(data[0].tag_name)
+                return true
+            } catch {
+                callback(null)
+                return false
+            }
+        })
+}
+
+getApps((apps) => {
+    console.log(apps)
+})
